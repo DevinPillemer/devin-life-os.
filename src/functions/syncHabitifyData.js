15845259d3ec52ef488be81724d07eb1381dcd23
DevinPillemer <devin.pillemer@gmail.com>
2026-02-14 17:31:23 +0200

    Create syncHabitifyData stub function

---
+++ src/functions/syncHabitifyData.js
@@ -0,0 +1,6 @@
+// Stub for Habitify sync function
+// This will be connected to the actual Habitify API later
+export async function syncHabitifyData() {
+  console.log('Habitify sync not yet configured');
+  return { success: false, message: 'Habitify sync not configured' };
+}
