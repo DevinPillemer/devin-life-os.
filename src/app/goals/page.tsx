e745ff8a3e7fd34329700cbfc6a72b9e385dda28
DevinPillemer <devin.pillemer@gmail.com>
2026-04-18 22:14:45 +0300

    Fix Next.js page rendering config exports

--- src/app/goals/page.tsx
+++ src/app/goals/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 export const fetchCache = "force-no-store";
 
